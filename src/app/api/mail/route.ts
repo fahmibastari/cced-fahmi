c9770924aaad6af7812f5cd61458739fb7b13ca6
fahmibastari <fahmibastari549@gmail.com>
2025-05-16 20:56:41 +0700

    wih susahnya

--- src/app/api/mail/route.ts
+++ src/app/api/mail/route.ts
@@ -1,31 +1,26 @@
 import { render } from "@react-email/components";
 import WelcomeEmail from "../../../../emails";
 import { Resend } from "resend";
+import { NextRequest } from "next/server";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY!);
 
-export async function POST(request: Request, res: Response) {
+export async function POST(request: NextRequest) {
     const { email, fullname } = await request.json();
 
-    // Tunggu dulu hasil render-nya (string HTML)
+    // Render email ke HTML
     const htmlContent = await render(WelcomeEmail({ fullname }));
 
     const { data, error } = await resend.emails.send({
         from: "Acme <fahmibastari549@gmail.com>",
         to: [email],
         subject: "Thank You",
-        html: htmlContent, // masukkan hasil render yang sudah di-resolve
+        html: htmlContent,
     });
 
-    // Kamu bisa return sesuatu jika perlu
+    // Return response JSON dengan status yang sesuai
     return new Response(JSON.stringify({ data, error }), {
         status: error ? 500 : 200,
         headers: { "Content-Type": "application/json" },
     });
-
-    if (error) {
-        return Response.json(error);   
-    }
-
-    return Response.json ({message: "Email Berhasil Terkirim."})
 }
