5c95558a0e501daa9ae92df686f06a8e13f627e2
fahmibastari <fahmibastari549@gmail.com>
2025-05-16 21:40:14 +0700

    lagihhhhhhhh

--- src/app/jobs/[id]/page.tsx
+++ src/app/jobs/[id]/page.tsx
@@ -1,18 +1,8 @@
 import { notFound } from 'next/navigation'
 import prisma from '@/lib/prisma'
 import JobDetailPublic from '@/components/public/JobDetailPublic'
-import { Params } from 'next/dist/server/request/params'
 
-interface Props {
-  params: {
-    id: string
-  }
-}
-type PageProps = {
-  params: Params & { id: string }
-}
-
-export default async function JobDetailPage({ params }: Props): Promise<JSX.Element> {
+export default async function JobDetailPage({ params }: { params: { id: string } }): Promise<JSX.Element> {
   const { id } = params
 
   const job = await prisma.job.findUnique({
