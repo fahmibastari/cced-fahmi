70e4552abd0e4cfefab31b80412783202330681a
fahmibastari <fahmibastari549@gmail.com>
2025-06-17 09:36:10 +0700

    logintss

--- src/data/user.ts
+++ src/data/user.ts
@@ -2,12 +2,23 @@ import prisma from '@/lib/prisma'
 
 export const getUserByEmail = async (email: string) => {
   try {
-    const user = await prisma.user.findUnique({ where: { email } })
+    const user = await prisma.user.findUnique({
+      where: { email },
+      select: {
+        id: true,
+        email: true,
+        fullname: true,
+        password: true,
+        emailVerified: true, // ✅ tambahkan ini
+        role: true,           // ✅ tambahkan ini
+      },
+    })
     return user
   } catch {
     return null
   }
 }
+
 
 export const getUserById = async (id: string) => {
   try {
